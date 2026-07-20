45b93e7bc550ba91b9c19fa92195169625ce11ad
crossway0945-ux <crossway0945@gmail.com>
2026-07-20 23:15:49 +0700

    Delete diagnostic-lab-v12-3-0-full-system-upgrade directory

--- diagnostic-lab-v12-3-0-full-system-upgrade/domain/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./task1Classification.js";
-export * from "./task2Safety.js";
-export * from "./canonicalAnalysis.js";
-export * from "./paragraphEvidence.js";
-export * from "./reportViewModels.js";
-export * from "./textIntegrity.js";
